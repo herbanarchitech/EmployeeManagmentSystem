088ef503b2f6ba38395fb31891174102f4197b61
herbanarchitech <jeff.badette@protonmail.com>
2021-10-25 03:25:59 -0400

    initial commit.Setting Environment

---
+++ src/test/java/com/example/captstone/CapstoneprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.captstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
